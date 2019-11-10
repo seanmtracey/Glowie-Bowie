bf8c211ad0489c042d72fd86cc98b861a75f4ef3
Sean Tracey <seanmtracey@gmail.com>
2019-11-10 05:43:49 +0000

    Simple MQTT client and setup script

---
+++ light-controller/main.py
@@ -0,0 +1,16 @@
+import paho.mqtt.client as mqtt
+
+# The callback for when the client receives a CONNACK response from the server.
+def on_connect(client, userdata, flags, rc):
+    print("Connected with result code " + str(rc))
+    client.subscribe("#")
+
+def on_message(client, userdata, msg):
+    print(msg.topic + " " + str(msg.payload))
+
+client = mqtt.Client()
+client.on_connect = on_connect
+client.on_message = on_message
+
+client.connect("mqtt.eclipse.org", 1883, 60)
+client.loop_forever()
