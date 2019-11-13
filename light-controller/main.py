ce941d36d173bf534d4d03bd422563ff0f558b6d
seanmtracey <seanmtracey@gmail.com>
2019-11-13 19:08:39 +0000

    Functions for setting pixel values and brightness

--- light-controller/main.py
+++ light-controller/main.py
@@ -1,12 +1,42 @@
 import paho.mqtt.client as mqtt
+import board
+import neopixel
+import json
 
-# The callback for when the client receives a CONNACK response from the server.
+pixels = neopixel.NeoPixel(board.D16, 36, brightness=0.5, auto_write = False, pixel_order = neopixel.RGBW)
+
+def display_lights(matrix):
+
+    if matrix is None:
+        
+        print("No matrix of light values has been passed to display")
+        return False
+    
+    if len(matrix) is 0:
+        pixels.fill( (0,0,0,0) )
+
+    for idx in range(0, 18):
+        pixels[idx] = matrix[idx]
+
+    pixels.show()
+
+def set_brightness(level):
+    pixels.brightness(level)
+
 def on_connect(client, userdata, flags, rc):
     print("Connected with result code " + str(rc))
-    client.subscribe("#")
+    client.subscribe("glowie-bowie/lights")
+    client.subscribe("glowie-bowie/brightness")
 
 def on_message(client, userdata, msg):
     print(msg.topic + " " + str(msg.payload))
+
+    if msg.topic == "glowie-bowie/lights":
+        display_lights(msg.payload)
+
+    if msg.topic == "glowie-bowie/brightness":
+        print("Set Brightness Levels")
+        set_brightness( float(msg.payload) )
 
 client = mqtt.Client()
 client.on_connect = on_connect
