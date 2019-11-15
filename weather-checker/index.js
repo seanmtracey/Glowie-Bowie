056d54dbda8e502657954cbbb1d998a6ddc5d704
Sean Tracey <seanmtracey@gmail.com>
2019-11-15 01:33:43 +0000

    Add MQTT client for publishing Brixton weather details.

--- weather-checker/index.js
+++ weather-checker/index.js
@@ -1,4 +1,5 @@
 const fetch = require('node-fetch');
+const mqtt = require('mqtt');
 
 const weather_api_host = "api.openweathermap.org";
 const weather_api_path = "data/2.5/weather";
@@ -7,23 +8,56 @@ function main(params){
 
     const weather_data_api_request_url = `https://${weather_api_host}/${weather_api_path}?lat=${params.latitude}&lon=${params.longitude}&unit=${ params.unit || "metric" }&APPID=${params.WEATHER_API_KEY}`;
 
-    return fetch(weather_data_api_request_url)
-        .then(res => {
-            if(res.ok){
-                return res.json();
-            } else {
-                throw res;
-            }
-        })
-        .then(response => {
-            console.log(response);
-            return { data : response };
-        })
-        .catch(err => {
-            
-            console.log(err);
+    const MQTT_CLIENT  = mqtt.connect(`mqtt://${params.MQTT_BROKER}`);
 
-            throw { "err" : err };
+    return new Promise( (resolve, reject) => {
+
+            MQTT_CLIENT.on('connect', function () {
+                resolve();
+            });
+
+            MQTT_CLIENT.on('error', function (err) {
+                reject();
+            });
+
+        })
+        .then(function(){
+
+            return fetch(weather_data_api_request_url)
+                .then(res => {
+                    if(res.ok){
+                        return res.json();
+                    } else {
+                        throw res;
+                    }
+                })
+                .then(response => {
+                    console.log(response);
+
+                    return new Promise( (resolve, reject) => {
+
+                        MQTT_CLIENT.publish('brixton-weather-report', JSON.stringify(response), (err) => {
+                            if(err){
+                                reject(err);
+                            } else {
+
+                                MQTT_CLIENT.end();
+
+                                resolve(response);
+                            }
+                        });  
+
+                    });
+
+                })
+                .catch(err => {
+                    
+                    console.log(err);
+
+                    throw { "err" : err };
+
+                })
+            ;
 
         })
     ;
