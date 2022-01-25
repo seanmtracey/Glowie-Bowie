00287426b0afca9eb16a7ffac842f5a1ccfb44c8
Sean M. Tracey <seanmtracey@gmail.com>
2022-01-25 05:24:17 +0000

    Hook into AWS broker and update lights based on MQTT payload

--- light-controller/main.py
+++ light-controller/main.py
@@ -1,53 +1,83 @@
 from dotenv import load_dotenv
-import paho.mqtt.client as mqtt
-import board, neopixel, json, os
+import board, neopixel, json, os, time
 
 load_dotenv(verbose = True)
 
-MQTT_BROKER = os.getenv("MQTT_BROKER")
+from AWSIoTPythonSDK.MQTTLib import AWSIoTMQTTClient
 
-if MQTT_BROKER is None:
-    print("No MQTT_BROKER environment variable has been set. Exiting")
-    sys.exit()
+MQTT_TOPIC = os.getenv("MQTT_TOPIC")
 
-pixels = neopixel.NeoPixel(board.D16, 36, brightness = 0.5, auto_write = False, pixel_order = neopixel.RGBW)
+# For certificate based connection
+myMQTTClient = AWSIoTMQTTClient(os.getenv("CLIENT_ID"))
+# For TLS mutual authentication
+myMQTTClient.configureEndpoint(os.getenv("MQTT_HOST"), 8883)
 
-def display_lights(matrix):
+pixel_pin = board.D18
+num_pixels = 120
+ORDER = neopixel.GRB
 
-    if matrix is None:
-        
-        print("No matrix of light values has been passed to display")
-        return False
-    
-    if len(matrix) is 0:
-        pixels.fill( (0,0,0,0) )
+pixels = neopixel.NeoPixel(
+    pixel_pin, num_pixels, brightness=0.2, auto_write=False, pixel_order=ORDER
+)
 
-    for idx in range(0, 18):
-        pixels[idx] = matrix[idx]
+
+def display_lights(start_pixel = 0, end_pixel = 0, color = (255, 255, 255)):
+
+    for idx in range(start_pixel, end_pixel):
+        pixels[idx] = color
 
     pixels.show()
 
 def set_brightness(level):
     pixels.brightness(level)
 
-def on_connect(client, userdata, flags, rc):
-    print("Connected with result code " + str(rc))
-    client.subscribe("glowie-bowie/lights")
-    client.subscribe("glowie-bowie/brightness")
+'''
+Example payload
 
-def on_message(client, userdata, msg):
-    print(msg.topic + " " + str(msg.payload))
+{'lightSettings': {'leftStrip': {'color': [150, 255, 255]}, 'rightStrip': {'color': [190, 255, 200]}}, 'time': 1643085608, 'weatherType': 'clouds'}
 
-    if msg.topic == "glowie-bowie/lights":
-        display_lights(msg.payload)
+'''
 
-    if msg.topic == "glowie-bowie/brightness":
-        print("Set Brightness Levels")
-        set_brightness( float(msg.payload) )
+def handlePayload(client, userdata, message):
+	print("Received a new message from topic: ", message.topic)
+	print("\t", json.loads(message.payload))
+	print("\n\n")
 
-client = mqtt.Client()
-client.on_connect = on_connect
-client.on_message = on_message
+	info = json.loads(message.payload)
+	print(info)
+	leftColorTuple = (info['lightSettings']['leftStrip']['color'][0], info['lightSettings']['leftStrip']['color'][1], info['lightSettings']['leftStrip']['color'][2])
+	rightColorTuple = (info['lightSettings']['rightStrip']['color'][0], info['lightSettings']['rightStrip']['color'][1], info['lightSettings']['rightStrip']['color'][2])
 
-client.connect(MQTT_BROKER, 1883, 60)
-client.loop_forever()
+	print("Left strip color will be:", leftColorTuple)
+	print("Right strip color will be:", rightColorTuple)
+
+	display_lights(0, 59, leftColorTuple)
+	display_lights(59, 79, rightColorTuple)
+
+
+myMQTTClient.configureCredentials(os.getenv("CA_PATH"), os.getenv("PRIVATE_KEY_PATH"), os.getenv("CERTIFICATE_PATH"))
+myMQTTClient.configureOfflinePublishQueueing(-1)
+myMQTTClient.configureDrainingFrequency(2)
+myMQTTClient.configureConnectDisconnectTimeout(10)
+myMQTTClient.configureMQTTOperationTimeout(5) 
+
+print("Connecting to AWS IoT Broker")
+myMQTTClient.connect()
+print("Subscribing to topic:", MQTT_TOPIC)
+myMQTTClient.subscribe(MQTT_TOPIC, 1, handlePayload)
+
+leftColorTuple = (255,0,0)
+rightColorTuple = (0,255,0)
+
+display_lights(0, 59, leftColorTuple)
+display_lights(60, 79, rightColorTuple)
+
+pixels.show()
+
+time.sleep(5)
+
+pixels.fill( (0,0,0,0) )
+pixels.show()
+
+while True:
+	time.sleep(1)
