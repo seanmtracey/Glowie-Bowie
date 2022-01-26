6602827ae6dd64c08efb85b4b9fecf4bf825b3d5
seanmtracey <seanmtracey@gmail.com>
2022-01-26 23:11:17 +0000

    Enable rainbow triggering from remote

--- light-controller/main.py
+++ light-controller/main.py
@@ -38,6 +38,42 @@ Example payload
 
 '''
 
+def wheel(pos):
+    # Input a value 0 to 255 to get a color value.
+    # The colours are a transition r - g - b - back to r.
+    if pos < 0 or pos > 255:
+        r = g = b = 0
+    elif pos < 85:
+        r = int(pos * 3)
+        g = int(255 - pos * 3)
+        b = 0
+    elif pos < 170:
+        pos -= 85
+        r = int(255 - pos * 3)
+        g = 0
+        b = int(pos * 3)
+    else:
+        pos -= 170
+        r = 0
+        g = int(pos * 3)
+        b = int(255 - pos * 3)
+    return (r, g, b) if ORDER in (neopixel.RGB, neopixel.GRB) else (r, g, b, 0)
+
+
+def rainbow_cycle(wait):
+    for j in range(255):
+        for i in range(num_pixels):
+            pixel_index = (i * 256 // num_pixels) + j
+            pixels[i] = wheel(pixel_index & 255)
+        pixels.show()
+        time.sleep(wait)
+
+def trigger_rainbow_cycle(client, userdata, message):
+    
+    print("Engaging rainbow cycle")
+
+    rainbow_cycle(0.010)
+
 def handlePayload(client, userdata, message):
 	print("Received a new message from topic: ", message.topic)
 	print("\t", json.loads(message.payload))
@@ -56,6 +92,8 @@ def handlePayload(client, userdata, message):
 	display_lights(78, 97, rightColorTuple)
 
 
+
+
 CA_PATH = os.path.join(os.path.dirname(__file__), os.getenv("CA_PATH"))
 PRIVATE_KEY_PATH = os.path.join(os.path.dirname(__file__), os.getenv("PRIVATE_KEY_PATH"))
 CERTIFICATE_PATH = os.path.join(os.path.dirname(__file__), os.getenv("CERTIFICATE_PATH"))
@@ -71,6 +109,7 @@ print("Connecting to AWS IoT Broker")
 myMQTTClient.connect()
 print("Subscribing to topic:", MQTT_TOPIC)
 myMQTTClient.subscribe(MQTT_TOPIC, 1, handlePayload)
+myMQTTClient.subscribe("glowie_bowie/lights/rainbow", 1, trigger_rainbow_cycle)
 
 leftColorTuple = (255,0,0)
 rightColorTuple = (0,255,0)
